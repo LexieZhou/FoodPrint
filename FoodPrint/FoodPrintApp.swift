28f1b77aa3953823085cca5c3f22628412e3fc28
LexieZhou <zihan2@connect.hku.hk>
2023-09-25 14:44:34 +0800

    Initial Commit

---
+++ FoodPrint/FoodPrintApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPrintApp.swift
+//  FoodPrint
+//
+//  Created by Lexie Zhou on 25/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodPrintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodPrintUITests/FoodPrintUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FoodPrintUITestsLaunchTests.swift
+//  FoodPrintUITests
+//
+//  Created by Lexie Zhou on 25/9/2023.
+//
+
+import XCTest
+
+final class FoodPrintUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
