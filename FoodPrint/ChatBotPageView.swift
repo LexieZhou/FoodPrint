3cb58c66e5399260598a05a595464d811bee9747
Boaoz <93388899+Boaoz@users.noreply.github.com>
2023-11-16 17:05:49 +0800

    Add files via upload

--- FoodPrint/ChatBotPageView.swift
+++ FoodPrint/ChatBotPageView.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftUI
 
 struct ChatbotPageView: View {
-    @State private var TOKEN: String = "" // DONOT PUSH ONTO GITHUB
+    @State private var TOKEN: String = "" // DO NOT PUSH ONTO GITHUB
     
     @State private var messageText = ""
     @State private var recordText = ""
@@ -30,11 +30,11 @@ struct ChatbotPageView: View {
                     recordText = recordText + "\(i), \(timestamp[i]), \(height[i]), \(weight[i]), \(foodCategory[i]), \(calories[i])\\n"
                 }
             } else {
-                for i in (0 ..< 50) {
+                for i in (0 ..< 20) {
                     recordText = recordText + "\(i), \(timestamp[i]), \(height[i]), \(weight[i]), \(foodCategory[i]), \(calories[i])\\n"
                 }
                 recordText = recordText + "...[PARTIAL DATA HIDDEN]...\\n"
-                for i in (timestamp.count - 50 ..< timestamp.count) {
+                for i in (timestamp.count - 80 ..< timestamp.count) {
                     recordText = recordText + "\(i), \(timestamp[i]), \(height[i]), \(weight[i]), \(foodCategory[i]), \(calories[i])\\n"
                 }
             }
@@ -54,7 +54,7 @@ struct ChatbotPageView: View {
     "messages": [
       {
         "role": "system",
-        "content": "You are a helpful Personal Diet Assistant providing diet advices to help the user. Your answers need to be concise with no more than 50 words. The user is practicing 16:8 intermittent fasting, which involves an 8-hour window for food consumption and fasting for 16 hours. Please make use of the following user record to come up with personalized advice. The record is in comma-separated format and in chronological order.\\n record_id, timestamp, user_height, user_weight, food_eaten, kilogram_calories_of_food_eaten\\n\(self.recordText)\\nThe current timestamp is 20/11/2023 12:14."
+        "content": "You are a helpful Personal Diet Assistant providing diet advices to help the user. Your answers need to be concise with no more than 50 words. The user is practicing 16:8 intermittent fasting, which involves an 8-hour window for food consumption and fasting for 16 hours. The 8-hour window starts upon the record of the first meal of the day. Please make use of the following user record to come up with personalized advice. The record is in comma-separated format and in chronological order.\\n record_id, timestamp, user_height, user_weight, food_eaten, kilogram_calories_of_food_eaten\\n\(self.recordText)\\nThe current timestamp is 20/11/2023 12:14."
       },
       \(messageThread(messages: messages))
     ]
@@ -81,7 +81,7 @@ struct ChatbotPageView: View {
             }
         }.resume()
         semaphore.wait()
-        return GPTResponse
+        return GPTResponse.replacingOccurrences(of: "\\n", with: "\n")
     }
     
     func messageThread(messages: [String]) -> String {
