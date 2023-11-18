59a118f4ea31d770710bdf2a62e0df0b9eac360d
LexieZhou <zihan2@connect.hku.hk>
2023-11-18 20:04:20 +0800

    modify database record structure

--- FoodPrint/DataManager.swift
+++ FoodPrint/DataManager.swift
@@ -37,23 +37,27 @@ class FirebaseDataManager {
     }
     static func writeRecord(record: Record) {
         let databaseRef = Database.database().reference()
-        let newRecordRef = databaseRef.child("Records").childByAutoId()
         
-        let recordData: [String: Any] = [
-            "UserId": record.userId,
-            "RecordID": record.recordId,
-            "Timestamp": record.timestamp,
-            "Weight": record.weight,
-            "Height": record.height,
-            "Food_category": record.foodCategory,
-            "Calories": record.calories
-        ]
-        
-        newRecordRef.setValue(recordData) { error, _ in
-            if let error = error {
-                print("Error writing record to Firebase: \(error.localizedDescription)")
-            } else {
-                print("Record written successfully.")
+        databaseRef.observeSingleEvent(of: .value) { snapshot in
+            let databaseLength = snapshot.childrenCount - 1
+            let newRecordRef = databaseRef.child("\(databaseLength)")
+            
+            let recordData: [String: Any] = [
+                "UserId": record.userId,
+                "RecordID": record.recordId,
+                "Timestamp": record.timestamp,
+                "Weight": record.weight,
+                "Height": record.height,
+                "Food_category": record.foodCategory,
+                "Calories": record.calories
+            ]
+            
+            newRecordRef.setValue(recordData) { error, _ in
+                if let error = error {
+                    print("Error writing record to Firebase: \(error.localizedDescription)")
+                } else {
+                    print("Record written successfully.")
+                }
             }
         }
     }
