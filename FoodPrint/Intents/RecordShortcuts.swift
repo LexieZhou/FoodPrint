1a83306386481caf0a4758a6d2bd768645943585
LexieZhou <zihan2@connect.hku.hk>
2023-11-18 13:16:58 +0800

    add siri intent

---
+++ FoodPrint/Intents/RecordShortcuts.swift
@@ -0,0 +1,27 @@
+//
+//  RecordShortcuts.swift
+//  FoodPrint
+//
+//  Created by Lexie Zhou on 18/11/2023.
+//
+
+import Foundation
+import AppIntents
+import Intents
+
+// 1
+struct RecordShortcuts: AppShortcutsProvider {
+  // 2
+  static var appShortcuts: [AppShortcut] {
+    AppShortcut(
+      intent: RecordIntent(),
+      phrases: [
+        "Record a meal",
+        "Open Foodprint and record a meal",
+        "Open Foodprint to record a meal"
+      ],
+      shortTitle: "Create a Meal Record",
+      systemImageName: "camera.fill"
+    )
+  }
+}
