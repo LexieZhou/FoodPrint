71fa5ad15b33715efd9a10e3957eda73a7f8db75
LexieZhou <zihan2@connect.hku.hk>
2023-12-03 12:39:39 +0800

    correct gpt connection model

--- FoodPrint/ChatBotPageView.swift
+++ FoodPrint/ChatBotPageView.swift
@@ -10,11 +10,9 @@ import SwiftUI
 
 struct ChatbotPageView: View {
     @State private var TOKEN: String = "" // DO NOT PUSH ONTO GITHUB
-        
     @State private var messageText = ""
     @State private var recordText = ""
     @State private var records: [Record] = []
-//    @State var messages: [String] = ["Welcome to FoodPrint Personal Diet Assistant!"]
     @StateObject var allMessages = Messages()
     @State private var showSheet: Bool = false
     @State private var showNotification: Bool = false
@@ -85,7 +83,6 @@ struct ChatbotPageView: View {
                     .cornerRadius(20)
                     .onSubmit {
                         if messageText != "" {
-//                            sendMessage(message: messageText, photo: nil)
                             allMessages.messages.append(Message(id: UUID(), message: "[USER]" + messageText))
                             allMessages.messages.append(Message(id: UUID(), message: getBotResponse(messages: allMessages.messages.map { $0.message })))
                             messageText = ""
@@ -95,7 +92,6 @@ struct ChatbotPageView: View {
                 // send message button
                 Button{
                     if messageText != "" {
-//                        sendMessage(message: messageText, photo: nil)
                         allMessages.messages.append(Message(id: UUID(), message: "[USER]" + messageText))
                         // get GPT response
                         allMessages.messages.append(Message(id: UUID(), message: getBotResponse(messages: allMessages.messages.map { $0.message })))
@@ -140,77 +136,16 @@ struct ChatbotPageView: View {
         }
     
     func dismissImagePicker() {
-        var GPTResponse: String = "Ramen, 300 kcal."
-        var GPTResponseParsed: (String, Int?) = ("Unrecognized food", 0)
-        var notificationString: String = "Ramen, 300 kcal."
         var base64String = ""
         if let imageData = image?.jpegData(compressionQuality: 0.1) {
             base64String = imageData.base64EncodedString()
         }
-        if base64String == "" {
-            notificationText = "No photo selected."
-            // showNotification = true
-            // do nothing
-        } else {
+        if base64String != "" {
             // image is not nil
             if let imageData = image?.jpegData(compressionQuality: 0.1) {
-                allMessages.writeMessage(id: UUID(), message: "[USER]", photo: imageData)
+                allMessages.writeMessage(id: UUID(), message: "[USER][IMG]"+base64String, photo: imageData)
             }
-            guard let url = URL(string: "https://api.openai.com/v1/chat/completions"),
-                  let payload = """
-                      {
-                        "model": "gpt-4-vision-preview",
-                        "max_tokens": 30,
-                        "messages": [
-                            {
-                                "role": "system",
-                                "content": [{"type": "text", "text": "You are a helpful and professional Diat Analyst."}]
-                            },
-                            {
-                                "role": "user",
-                                "content": [
-                                    {
-                                        "type": "text",
-                                        "text": "Read the photo of a meal. Please responde with minimal number of words (at most 3 words) describing what food it is, and a number (not a range) indicating the estimated energy this meal includes in kilogram calories. Format the response as: [FOOD] @ [ENERGY] kcal."
-                                    },
-                                    {
-                                        "type": "image_url",
-                                        "image_url": {
-                                            "url": "data:image/jpeg;base64,{\(base64String)}",
-                                            "detail": "low"
-                                        }
-                                    }
-                                ]
-                            }
-                        ]
-                      }
-                    """.data(using: .utf8) else{return}
-
-                    var request = URLRequest(url: url)
-                    request.httpMethod = "POST"
-                    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-                    request.addValue("Bearer \(TOKEN)", forHTTPHeaderField: "Authorization")
-                    request.httpBody = payload
-                    let semaphore = DispatchSemaphore(value: 0)
-                    URLSession.shared.dataTask(with: request) { (data, response, error) in
-                        defer { semaphore.signal() }
-                        guard error == nil else { print(error!.localizedDescription); return }
-                        guard let data = data else { print("Empty data"); return }
-                        if let str = String(data: data, encoding: .utf8) {
-                            print(str)
-                            GPTResponse = String(String(str.components(separatedBy: "\"}, \"finish_details\": ")[0]).components(separatedBy: "content\": \"")[1])
-                            GPTResponseParsed = GPTResponseParser(GPTResponse: GPTResponse)
-                            notificationString = GPTResponseParsed.0 + ", " +  String(GPTResponseParsed.1 ?? 0) + " kcal."
-    //                        GPTResponseParsed = GPTResponseParser(GPTResponse: GPTResponse).0 + ", " +  String(GPTResponseParser(GPTResponse: GPTResponse).1 ?? 0) + " kcal."
-                            print(GPTResponse)
-                            print(GPTResponseParsed)
-                            }
-                        }.resume()
-                        semaphore.wait()
-                    // GPT respond
-                    allMessages.writeMessage(id: UUID(), message: notificationString, photo: nil)
-//                        notificationText = notificationString
-//                        showNotification = true
+            allMessages.writeMessage(id: UUID(), message: getBotResponse(messages: allMessages.messages.map { $0.message }), photo: nil)
                     }
     }
     
@@ -238,30 +173,30 @@ struct ChatbotPageView: View {
             }
         }
     }
-    
+
     func getBotResponse(messages: [String]) -> String {
         retrieveRecords()
+        let _ = print("last message:")
+        let _ = print(messages[messages.count - 1].replacingOccurrences(of: "[USER]", with: ""))
+        let _ = print("all message:")
+        let _ = print(messages)
         var GPTResponse: String = "That's cool!"
         
         guard let url = URL(string: "https://api.openai.com/v1/chat/completions"),
             let payload = """
-  {
-    "model": "gpt-4-1106-preview",
-    "temperature": 0.7,
-    "max_tokens": 1000,
-    "messages": [
-      {
-        "role": "system",
-        "content": "You are a helpful Personal Diet Assistant providing diet advice to help the user. Your answers need to be concise with no more than 50 words. The user is practicing 16:8 intermittent fasting, which involves an 8-hour window for food consumption and fasting for 16 hours. The 8-hour window starts upon the record of the first meal of the day. Please make use of the following user record to come up with personalized advice. The record is in comma-separated format and in chronological order.\\n record_id, timestamp, user_height, user_weight, food_eaten, kilogram_calories_of_food_eaten\\n\(self.recordText)\\nThe current timestamp is 20/11/2023 12:14."
-      },
-      \(messageThread(messages: messages))
-    ]
-  }
-""".data(using: .utf8) else
-        {
-            return "Error"
-        }
-        
+              {
+                "model": "gpt-4-vision-preview",
+                "max_tokens": 1000,
+                "messages": [
+                  {
+                    "role": "system",
+                    "content": "You are a helpful Personal Diet Assistant providing diet advice to help the user. Your answers need to be concise with no more than 50 words. The user is practicing 16:8 intermittent fasting, which involves an 8-hour window for food consumption and fasting for 16 hours. The 8-hour window starts upon the record of the first meal of the day. Please make use of the following user record to come up with personalized advice. The record is in comma-separated format and in chronological order.\\n record_id, timestamp, user_height, user_weight, food_eaten, kilogram_calories_of_food_eaten\\n\(self.recordText)\\nThe current timestamp is 20/11/2023 12:14."
+                  },
+                  \(messageThread(messages: messages))
+                ]
+              }
+            """.data(using: .utf8) else{return "Error"}
+    
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -274,7 +209,8 @@ struct ChatbotPageView: View {
             guard let data = data else { print("Empty data"); return }
             if let str = String(data: data, encoding: .utf8) {
                 print(str)
-                GPTResponse = String(String(String(str.components(separatedBy: "\n")[10]).components(separatedBy: "\"content\": ")[1]).dropLast().dropFirst())
+//                GPTResponse = String(String(String(str.components(separatedBy: "\n")[10]).components(separatedBy: "\"content\": ")[1]).dropLast().dropFirst())
+                GPTResponse = String(String(str.components(separatedBy: "\"}, \"finish_details\": ")[0]).components(separatedBy: "content\": \"")[1])
                 print(GPTResponse)
             }
         }.resume()
@@ -287,12 +223,33 @@ struct ChatbotPageView: View {
         var resArray: [String] = []
         for i in (0 ..< messages.count) {
             if messages[i].contains("[USER]"){
-                resArray.append("""
+                if messages[i].contains("[USER][IMG]"){
+                    resArray.append("""
+                    {
+                        "role": "user",
+                        "content": [
+                            {
+                                "type": "text",
+                                "text": "Read the this photo."
+                            },
+                            {
+                                "type": "image_url",
+                                "image_url": {
+                                    "url": "data:image/jpeg;base64,{\(messages[i].replacingOccurrences(of: "[USER][IMG]", with: ""))",
+                                    "detail": "low"
+                                }
+                            }
+                        ]
+                    }
+                    """)
+                } else {
+                    resArray.append("""
                   {
                     "role": "user",
                     "content": "\(messages[i].replacingOccurrences(of: "[USER]", with: ""))"
                   }
             """)
+                }
                         } else {
                             resArray.append("""
                   {
@@ -305,6 +262,7 @@ struct ChatbotPageView: View {
         resStr = resArray.joined(separator: ",\n")
         return resStr
     }
+
     func GPTResponseParser(GPTResponse: String) -> (String, Int?) {
         if GPTResponse.contains("@") {
             do {
@@ -319,6 +277,7 @@ struct ChatbotPageView: View {
         }
     }
 }
+
 struct Message : Identifiable {
     var id : UUID
     var message: String
